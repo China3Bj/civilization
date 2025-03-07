5cddeead1495a1e346fca7bd6dad0a5e088f1663
MAHTMATCIS <tongmx33@163.com>
2025-03-07 20:57:03 +0800

    prepare v0.0.MAHT

--- base.py
+++ base.py
@@ -1,13 +1,58 @@
 import pygame
 import tomllib
 import numba
+import json
 print('numbaVer:',numba.__version__)
 pygame.init()
 print('pygameVer:',pygame.__version__)
-__all__=['pygame','config','numba','tomllib']
+erronBuffer= (b'\x00\x00\x00\x00\x00\x00\xff\x00\xff\xff\x00\xff\x00\x00\x00\x00\x00\x00\xff\x00\xff\xff\x00\xff\xff'
+              b'\x00\xff\xff\x00\xff\x00\x00\x00\x00\x00\x00\xff\x00\xff\xff\x00\xff\x00\x00\x00\x00\x00\x00')
+
+class AssetsDict(dict):
+    def __getattr__(self, item):
+        __g=self[item]
+        if type(__g)==dict:
+            __g=AssetsDict(__g)
+        return __g
+    def __getitem__(self, item):
+        try:
+            return super().__getitem__(item)
+        except:
+            _a=pygame.image.frombytes(erronBuffer,(4,4),"RGB")
+            return _a
+
+class ConfigDict(dict):
+    def __getattr__(self, item):
+        __g=self[item]
+        if type(__g)==dict:
+            __g=ConfigDict(__g)
+        return __g
+
 with open('config.toml', 'rb') as f:
     try:
-        config=tomllib.load(f)
+        config=ConfigDict(tomllib.load(f))
+        __ass=config['local']['assetsLink']
+        with open(__ass, 'rb') as f2:
+            assetsLink=AssetsDict(json.load(f2))
+            print(assetsLink)
     except:
-        pass
+        raise FileNotFoundError
 
+
+
+class PyButton:
+    def __init__(self,surface:pygame.Surface,x,y,length):
+        self.surface=surface
+        self.x=x
+        self.y=y
+        self.length=length
+        self.__leftImg=assetsLink.texture.btn.left
+        self.__middleImg=assetsLink.texture.btn.middle
+        self.__rightImg=assetsLink.texture.btn.right
+
+    def update(self):
+        self.surface.blit(self.__leftImg,(self.x,self.y))
+
+if __name__=="__main__":
+    s=pygame.surface.Surface((100,100))
+    PyButton(s,1,1,1)
--- main.py
+++ main.py
@@ -3,26 +3,6 @@ import pygame
 import random
 import sys
 
-class FirstBlock:
-    def __init__(self,surface):
-        self.surface=surface
-        self.pos=pygame.Vector2(0,0)
-        self.step=pygame.Vector2(random.uniform(1.,8.),random.uniform(1.,8.))
-        self.size=pygame.Vector2(50,50)
-    def update(self):
-        pygame.draw.rect(self.surface,'white', [self.pos,self.size])
-
-        if not 0 < self.pos[0]:
-            self.step[0] = max(-self.step[0], self.step[0])
-        if not self.pos[0]+self.size[0] < config['window']['weight']:
-            self.step[0] = min(-self.step[0], self.step[0])
-        if not 0<self.pos[1]:
-            self.step[1]=max(-self.step[1],self.step[1])
-        if not self.pos[1]+self.size[1]<config['window']['height']:
-            self.step[1]=min(-self.step[1],self.step[1])
-
-        self.pos+=self.step
-
 class MainGame:
     def __init__(self,*args,**kwargs):
         self.__args=args
@@ -30,12 +10,15 @@ class MainGame:
         self.surface=pygame.display.set_mode((config['window']['weight'],config['window']['height']))
         self.clock=pygame.time.Clock()
 
-        block=[FirstBlock(self.surface) for _ in range(20)]
+        self.welcomeActive=True
+        a=PyButton(self.surface,100,100,100)
 
         while True:
             self.surface.fill('black')
-            for _ in block:
-                _.update()
+
+            if self.welcomeActive:
+                a.update()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
